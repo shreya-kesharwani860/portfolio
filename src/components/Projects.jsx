b3aee5f518613bd0d4a8ddf652da5012e0ad36cb
Shreya Kesharwani <kesharwanisr_2@rknec.edu>
2026-03-06 01:36:38 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import ProjectCard from "./ProjectCard";
 import BlogAgent from "../assets/BlogAgent.jpg";
 import ChatbotImg from "../assets/ChatBot.jpg";
-import image.png from "../assets/image.png"
+import image from "../assets/image.png"
 
 const Projects = () => {
   return (
